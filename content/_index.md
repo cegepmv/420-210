8a3335f4efcc6511ba412d0f9ee3aec342803cd9
Samuel Fostiné <samuel.fostine@cegepmv.ca>
2026-01-21 23:58:15 -0500

    Squelette du site 420-210

---
+++ content/_index.md
@@ -0,0 +1,38 @@
++++
+title = "Programmation orientée objet"
+type = "home"
++++
+
+
+***
+
+## Plans de cours
+
+:clipboard: Samuel Fostiné  : <a href="/420-210/Plan_de_cours_420_210_MV_SAMUEL.pdf" target="_blanck">Plan de cours</a>
+
+
+***
+
+## Horaire et disponibilités des enseignants
+
+:heavy_check_mark: Samuel Fostiné  : <a href="/420-210/horaire_hiver2026.pdf" target="_blanck">Lien vers mon horaire et mes disponibilités</a>
+
+***
+
+##  Samuel Fostiné 
+
+|Nom de l'évaluation|Pondération|Semaine de l'évaluation|
+|:---:|:---:|:---:|
+|10 quiz (Wooclap) (Les 8 meilleures quiz)| 10 % |Semaine 2 à 14|
+|Examen 1| 15% |Semaine 5|
+|Examen 2| 20 % |Semaine 11|
+|Laboratoire| 10 % |Semaine 8|
+|Projet remise final| 15 % |Semaine 14|
+|Examen final| 30 % |Semaine 15|
+
+
+
+
+
+
+
---
+++ public/searchindex.en.js
@@ -0,0 +1,26 @@
+var relearn_searchindex = [
+  {
+    "breadcrumb": "Programmation orientée objet",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Categories",
+    "uri": "/420-210/categories/index.html"
+  },
+  {
+    "breadcrumb": "",
+    "content": "Plans de cours 📋 Samuel Fostiné : Plan de cours\nHoraire et disponibilités des enseignants ✔️ Samuel Fostiné : Lien vers mon horaire et mes disponibilités\nSamuel Fostiné Nom de l’évaluation Pondération Semaine de l’évaluation 10 quiz (Wooclap) (Les 8 meilleures quiz) 10 % Semaine 2 à 14 Examen 1 15% Semaine 5 Examen 2 20 % Semaine 11 Laboratoire 10 % Semaine 8 Projet remise final 15 % Semaine 14 Examen final 30 % Semaine 15",
+    "description": "Plans de cours 📋 Samuel Fostiné : Plan de cours\nHoraire et disponibilités des enseignants ✔️ Samuel Fostiné : Lien vers mon horaire et mes disponibilités\nSamuel Fostiné Nom de l’évaluation Pondération Semaine de l’évaluation 10 quiz (Wooclap) (Les 8 meilleures quiz) 10 % Semaine 2 à 14 Examen 1 15% Semaine 5 Examen 2 20 % Semaine 11 Laboratoire 10 % Semaine 8 Projet remise final 15 % Semaine 14 Examen final 30 % Semaine 15",
+    "tags": [],
+    "title": "Programmation orientée objet",
+    "uri": "/420-210/index.html"
+  },
+  {
+    "breadcrumb": "Programmation orientée objet",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Tags",
+    "uri": "/420-210/tags/index.html"
+  }
+]
